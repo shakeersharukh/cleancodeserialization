aa4521a2ef311773f4fe92c982492436cb300363
shakeersharukh <60890562+shakeersharukh@users.noreply.github.com>
2020-07-26 17:42:30 +0530

    Create house.java

---
+++ house.java
@@ -0,0 +1,55 @@
+class Area
+{
+    private double s;
+    private double a;
+
+    public void setSqfeet(double s)
+    {
+        this.s=s;
+    }
+
+    public double getSqfeet(double s)
+    {
+        return this.s;  
+    }
+
+    public void setArea(double a)
+    {
+        this.a=a; 
+    }
+
+    public double getArea() 
+    {
+        if(this.s==1200)   
+        {
+            return (this.a)/(1200*this.s); 
+        } 
+
+        else if(this.s==1500)   
+        {
+            return (this.a)/(1500*this.s);
+        }
+
+        else if(this.s==1800)
+        {
+            return (this.a)/(1800*this.s);
+        }
+
+        else  
+        {
+            return (this.a)/(2500*this.s);
+        }
+    }
+}
+
+public class Main
+{	
+	public static void main(String[] args)
+	{
+		Area ar=new Area(); 
+    		ar.setSqfeet(1500);
+                ar.setArea(2589000);
+                System.out.println(ar.getArea());
+                //System.out.println(ar.getSqfeet());	
+   }
+}
